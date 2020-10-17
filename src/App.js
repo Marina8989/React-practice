8a6de04a036898a646e4443cd40444157def2cb8
Marina8989 <marina.mill209@gmail.com>
2020-10-17 16:42:35 -0700

    React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import {add, multiply} from './helpers';
-import cats, { meow } from './cats'
+// import logo from './logo.svg';
+import ShoppingCard from './ShoppingCart';
+import items from './items';
+
+// import {add, multiply} from './helpers';
+// import cats, { meow } from './cats'
 import './App.css';
 
 function App() {
-  console.log(multiply(4, 9));
-  console.log(cats);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HI I MADE A CHANGE!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <ShoppingCart items = { items } />
   );
 }
 
