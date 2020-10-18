d3425f921ce79bc125f9d280ebcd0cb60ccf9410
Marina8989 <marina.mill209@gmail.com>
2020-10-17 18:56:36 -0700

    Reract

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const CartItem = () => {
-
-}
+const CartItem = ({name, img, price, quantity}) => (
+    <div>
+        <h4>{name}</h4>
+        <img src={img} width="200"/>
+        <ul>
+            <li>Price: ${price}</li>
+            <li>Quantity: {quantity}</li>
+            <li>Subtotal: ${price * quantity}</li>
+        </ul>
+    </div>
+)
 
 export default CartItem;
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,21 +1,19 @@
 import React from "react";
+import CartItem from './CartItem'
 
 const ShoppingCart = ({ items, username }) => {
+  const total = items.reduce((acc, i) => {
+       return acc + i.price * i.quantity
+  }, 0)
      return (
-     <div>
-           <h1>{username}'s Shopping Cart</h1>
-         <div>
-              {items.map(i => (
-            <div>
-                <h4>{i.name}</h4>
-                <img src={i.img} width="200"/>
-             <ul>
-               <li>Price: ${i.price}</li>
-               <li>Quantity: {i.quantity}</li>
-             </ul>
-            </div>
-        ))}
+        <div>
+            <h1>{username}'s Shopping Cart</h1>
+           <div>
+             {items.map(i => (
+             <CartItem item={i.name} img={i.img} price={i.price} quantity={i.quantity}/>
+         ))}
        </div>
+       <b>Cart Total: ${ total }</b>
      </div>
      )
 }
